ba59a2973c80d1986079283753c0e39b9ea3ab47
Joabe <joabe.de.silva@gmail.com>
2023-06-06 20:27:33 -0300

    terminei a criação do preloader usando javascript: event listener e setTimeout

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 var icone = document.getElementById('icone');
+var logo = document.getElementById('logo)');
 
 icone.onclick = ()=> {
     document.body.classList.toggle('dark__theme');
 
     if (document.body.classList.contains('dark__theme')) {
         icone.src = "/SPA-personal-portfolio/assets/sun.png";
+        logo.src = "/SPA-personal-portfolio/assets/j(3).png"
     } else {
         icone.src = "/SPA-personal-portfolio/assets/moon.png";
     }
 }
+
+
+var loader = document.getElementById('preloader');
+
+window.addEventListener('load', function(load) {
+    window.removeEventListener('load', load, false);               
+    setTimeout(function(){
+        loader.style.display = 'none'
+    },2000);
+  
+  },false);
